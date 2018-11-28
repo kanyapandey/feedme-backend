f8fb85459b02ee8231312654efad4e7cad1c2d5a
kanyapandey <kanyapandey2603@gmail.com>
2018-11-28 12:40:31 +0700

    Fix update user

--- app.js
+++ app.js
@@ -20,8 +20,6 @@ mongoose.connection.on('error',(err) => {
 // app.use(cors())
 app.use(cors())
 
-//app.use(function(req, res, next) { res.header("Access-Control-Allow-Origin", "*"); res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); next(); });
-// app.use(function(req, res, next) { res.header("Access-Control-Allow-Origin", "*"); res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); next(); });
 app.use(function(req, res, next) { res.header("Access-Control-Allow-Origin", "*"); res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); next(); });
 
 app.use(validate());

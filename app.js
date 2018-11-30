7cc8abf9fc4cde9309aacd02365300c72a8d2a57
kanyapandey <kanyapandey2603@gmail.com>
2018-11-30 17:51:33 +0700

    fix hr

--- app.js
+++ app.js
@@ -19,11 +19,12 @@ mongoose.connection.on('error',(err) => {
 });
 
 
-
-
-
-
 app.use(cors())
+
+
+
+
+// app.use(cors())
 
 
 app.use(function(req, res, next) { res.header("Access-Control-Allow-Origin", "*"); res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); next(); });

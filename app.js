d22663b2b3d64968861ebe37e6bae3f4ede9417f
kanyapandey <kanyapandey2603@gmail.com>
2018-12-04 14:45:11 +0700

    fff

--- app.js
+++ app.js
@@ -19,6 +19,9 @@ mongoose.connection.on('error',(err) => {
 });
 
 app.use(cors())
+// app.use(function(req, res, next) { res.header("Access-Control-Allow-Origin", "*"); res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); next(); });
+
+
 app.use(function(req, res, next) { res.header("Access-Control-Allow-Origin", "*"); res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); next(); });
 
 app.use(validate());
